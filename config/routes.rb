46132208957789aa1b7dbab39cb6f3044f2134b8
Pavel Laletin <laletin@yahoo.com>
2014-03-23 01:15:27 +0700

    test

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Apps::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+  ###root :to => 'index#index'
   # root 'welcome#index'
 
   # Example of regular route:
